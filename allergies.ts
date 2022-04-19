8ec34cca876ff64f022009654cf7f1021b91f873
gness1804 <graham.nessler@gmail.com>
2022-04-19 11:28:55 -0500

    refactor: solve the allergicTo problem using binary

--- allergies.ts
+++ allergies.ts
@@ -1,12 +1,4 @@
-const allergies = new Map<string, number>();
-allergies.set('eggs', 1);
-allergies.set('peanuts', 2);
-allergies.set('shellfish', 4);
-allergies.set('strawberries', 8);
-allergies.set('tomatoes', 16);
-allergies.set('chocolate', 32);
-allergies.set('pollen', 64);
-allergies.set('cats', 128);
+const allergies = ['cats', 'pollen', 'chocolate', 'tomatoes', 'strawberries', 'shellfish', 'peanuts', 'eggs']
 
 export class Allergies {
   allergenIndex: number
@@ -63,31 +55,20 @@ export class Allergies {
     })
   }
 
-  public allergicTo(allergen: string): boolean {
-    if (this.allergenIndex === 0) return false;
-    // remove all scores that are greater than the allergen index
-    const scoresInRange = new Map([...allergies].filter(([allergy, score]) => score <= this.allergenIndex))
-    const scoresArr = [...scoresInRange];
-    if (scoresArr.length === 1) {
-      return !!scoresArr.find(elem => elem[1] === this.allergenIndex)
-    }
-    for (let i = scoresArr.length - 1; i >= 0; i--) {
-      const topElement = scoresArr[i];
-      const solutionsList: Array<[string, number]> = [topElement];
-      for (let j = scoresArr.length - 2; j >= 0; j--) {
-        const bottomElement = scoresArr[j];
-        // the sum is more than the allergenIndex; just move on and don't do anything
-        if (topElement[1] + bottomElement[1] > this.allergenIndex) {
-          continue;
-          // the sum equals the allergenIndex; we have our solution.
-        } else if (this.findSum(solutionsList) + bottomElement[1] === this.allergenIndex) {
-          solutionsList.push(bottomElement);
-          return !!solutionsList.find(elem => elem[0] === allergen)
-        } else if (topElement[1] + bottomElement[1] < this.allergenIndex) {
-          solutionsList.push(bottomElement);
-        }
+  private getAllergens(): string[] {
+    let result: string[] = [];
+    const binaryIndex = this.allergenIndex.toString(2);
+    const paddedIndex = binaryIndex.padStart(8, '0')
+    paddedIndex.split('').forEach((val, i) => {
+      if (parseInt(val)) {
+        result.push(allergies[i]);
       }
-    }
-    return false;
+    })
+    return result;
+  }
+
+  public allergicTo(allergen: string): boolean {
+    const allergens = this.getAllergens();
+    return allergens.includes(allergen);
   }
 }
